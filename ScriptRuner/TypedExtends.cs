dc3ce13a28024c12b8843ad4f3aeabef27da4a90
liu.yandong.hanks <liu.yandong.hanks@outlook.com>
2024-09-20 23:47:18 +0800

    1

---
+++ ScriptRuner/TypedExtends.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptRuner
+{
+    public static class TypedExtends
+    {
+
+ 
+
+ 
+        public static String add(this String _this, String value)
+        {
+            return _this + value;
+        }
+        public static IEnumerable<MethodInfo> GetExtensionMethods(Assembly assembly, Type extendedType)
+        {
+            var query = from type in assembly.GetTypes()
+                        where !type.IsGenericType && !type.IsNested
+                        from method in type.GetMethods(BindingFlags.Static
+                            | BindingFlags.Public | BindingFlags.NonPublic)
+                        where method.IsDefined(typeof(System.Runtime.CompilerServices.ExtensionAttribute), false)
+                        where method.GetParameters()[0].ParameterType == extendedType
+                        select method;
+            return query;
+        }
+
+    }
+}
